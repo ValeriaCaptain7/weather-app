a0f4d7d8795b978401dcf7f20ec94d107c8bb846
ValeriaCaptain7 <pantera3072@gmail.com>
2022-12-18 19:47:19 +0100

    Styling weather forecast CSS

--- styles/index.js
+++ styles/index.js
@@ -55,7 +55,7 @@ function displayForecast() {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "667d9f573c8af4c33457be5d561a9148";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
   
 }
